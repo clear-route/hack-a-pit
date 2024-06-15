390a4078cc89168d80d5479057872352d7ce079c
Lucas Marchi <lucas.marchi@clearroute.io>
2024-06-15 21:35:03 +0100

    fixed classes

--- src/logic/Car.ts
+++ src/logic/Car.ts
@@ -1,15 +1,38 @@
 import { Tyre } from "./Tyre";
+import { Driver } from "./Driver";
+import assert from "assert";
 
-class Car {
-  constructor(private name: string,  private driversList: []) {
-    this.driversList = driversList;
+export class Car {
+  constructor(private name: string, private driverList: Driver[]) {}
+
+  private fuelLeft: number = 100;
+  private currentDriver: Driver;
+
+  public setCurrentDriver(driver: Driver): void {
+    this.currentDriver = driver;
   }
-  fuelLeft: number = 100;
 
+  public getCurrentDriver(): Driver {
+    return this.currentDriver;
+  }
 
-  lastServiceTimestamp: number = 0;
-  tyresSet: Tyre = [];
+  public getFuelLeft(): number {
+    return this.fuelLeft;
+  }
 
+  public burnFuelBy(amount: number): void {
+    assert(amount < this.getFuelLeft(), "Ammout musst be less than fuel left.");
+    this.fuelLeft = this.fuelLeft - amount;
+  }
 
+  private tyresSet: Tyre[] = [];
+
+  public setTyresSet(): void {
+    this.tyresSet = [
+      new Tyre("soft1"),
+      new Tyre("soft2"),
+      new Tyre("soft3"),
+      new Tyre("soft4"),
+    ];
+  }
 }
-
