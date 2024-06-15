67fdfd3fadd3eed65398e11b6d70394457c6e21b
Lucas Marchi <lucas.marchi@clearroute.io>
2024-06-15 23:42:17 +0100

    changes to classes

--- src/logic/Driver.ts
+++ src/logic/Driver.ts
@@ -1,17 +1,22 @@
 export class Driver {
-  constructor(private name: string, private timeToExitRaceInMiutes: number) {
-    this.name = name;
-  }
-
+  private driverRemainingTimeInMinutes: number;
   private healthLeft: number = 100;
 
-  //TODO: Who holds this data? Race "rules" is what decides the max race time
-
-  public getTimeToExitRaceInMiutes(): number {
-    return this.timeToExitRaceInMiutes;
+  constructor(private name: string, private timeToExitRaceInMiutes: number) {
+    this.name = name;
+    this.driverRemainingTimeInMinutes = this.timeToExitRaceInMiutes;
   }
 
   public getHealthLeft(): number {
     return this.healthLeft;
+  }
+
+  public upHealthLeftBy(amount: number): void {
+    this.healthLeft =
+      this.healthLeft + amount <= 100 ? this.healthLeft + amount : 100;
+  }
+
+  public getDriverRemainingTimeInMinutes(): number {
+    return this.driverRemainingTimeInMinutes;
   }
 }
--- src/logic/Optimiser.ts
+++ src/logic/Optimiser.ts
@@ -3,66 +3,52 @@ import { Car } from "./Car";
 import { Race } from "./race";
 
 // Carries out the logic of the simulation
-class Optimiser {
-  // The simulation
-  constructor() {
-    this.simulation = new Simulation();
-  }
-  // Run the simulation
-  run() {
-    this.simulation.run();
-  }
 
-  //  a Race is created "Le Mans",
-  // Race rules are entered (?)
-  // RaceTime for Driver
-  // Number of tyre changes allowed by the race
-  // The Race has a Car
-  // The Car has a Driver(s)
+// // The simulation
+// constructor() {
+//   this.simulation = new Simulation();
+// }
+// // Run the simulation
+// run() {
+//   this.simulation.run();
+// }
 
-  // A Driver is created with:
-  // Name
-  // Hours Raced so Far (?)
-  // Health ininitialised at 100
-  // etc
+// Set Race
+let currentRace: Race = Race.getInstance("Le Mans", 240, 56);
 
-  public initialiseRace(): void {
-    // Set Race
-    let currentRace: Race = Race.getInstance("Le Mans", 240, 56);
+let maxHoursPerDriver = currentRace.getmaxHoursPerDriver();
 
-    let maxHoursPerDriver = currentRace.getmaxHoursPerDriver();
+// setDrivers
+let driver1 = new Driver("Driver 1", maxHoursPerDriver);
+let driver2 = new Driver("Driver 2", maxHoursPerDriver);
+let driver3 = new Driver("Driver 3", maxHoursPerDriver);
+let driver4 = new Driver("Driver 4", maxHoursPerDriver);
 
-    // setDrivers
-    let driver1 = new Driver("Driver 1", maxHoursPerDriver);
-    let driver2 = new Driver("Driver 2", maxHoursPerDriver);
-    let driver3 = new Driver("Driver 3", maxHoursPerDriver);
-    let driver4 = new Driver("Driver 4", maxHoursPerDriver);
+// setCars
+let car1 = new Car("Car 1", [driver1, driver2, driver3, driver4], driver1);
 
-    // setCars
+// Migth delete this
+car1.setLastServiceTimestamp();
 
-    let car1 = new Car("Car 1", [driver1, driver2, driver3, driver4], driver1);
+// Upon Driver Change:
+// Set the current driver
+// TODO: BUT CHECK HOW MUCH RACE TIME THE DRIVER HAS LEFT FIRST?
+// Set the last service timestamp, etc
 
-    // Migth delete this
-    car1.setLastServiceTimestamp();
+// Upon Tyre Change:
+// Set the new tyres set
+// Set the last service timestamp, etc
+// TODO: Update the number of tyre changes left !!!
 
-    // Upon Driver Change:
-    // Set the current driver
-    // Set the last service timestamp, etc
+//TODO: Create Expected Laps Count?
 
-    // Upon Tyre Change:
-    // Set the new tyres set
-    // Set the last service timestamp, etc
-    // TODO: Update the number of tyre changes left !!!
+// Upon Fuel Burn:
+// Set the fuel left
 
-    // Upon Fuel Burn:
-    // Set the fuel left
+// Upon Stint Time:
+// Set the stint time
 
-    // Upon Stint Time:
-    // Set the stint time
+// Upon Health Left:
+// Set the health left
 
-    // Upon Health Left:
-    // Set the health left
-
-    // Upon
-  }
-}
+// Upon
