eb73bcc12b4d830184efff89b6cc59cee31a2ec2
Matt Johnson <matt.johnson@clearroute.co.uk>
2024-06-15 22:23:24 +0100

    chore: mouse over lines, displays data

--- src/components/PitStop.js
+++ src/components/PitStop.js
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Rnd } from "react-rnd";
 import { Box, Typography } from "@mui/material";
 import "../css/PitStop.css";
 
-const PitStop = ({ pitStop, onDragStop }) => {
+const PitStop = ({ pitStop, onDragStop, trackWidth }) => {
+  const [hover, setHover] = useState(false);
+  const [show, setShow] = useState(false);
+  let hoverTimeout;
+
+  const calculateTime = (lap) => {
+    const hours = Math.floor(lap);
+    const minutes = Math.floor((lap - hours) * 60);
+    return `${hours}:${minutes < 10 ? "0" : ""}${minutes}`;
+  };
+
+  const handleMouseEnter = () => {
+    clearTimeout(hoverTimeout);
+    setHover(true);
+    setShow(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+    hoverTimeout = setTimeout(() => setShow(false), 1000);
+  };
+
+  useEffect(() => {
+    return () => clearTimeout(hoverTimeout);
+  }, []);
+
   return (
     <Rnd
       bounds="parent"
-      position={{ x: pitStop.lap, y: 150 }}
+      position={{ x: (pitStop.lap / 24) * trackWidth, y: 200 }}
       onDragStop={(e, d) => onDragStop(e, d, pitStop.id)}
       enableResizing={false}
       dragAxis="x"
+      className="pit-stop-container"
     >
       <Box
-        className="pit-stop"
-        sx={{
-          backgroundColor: "primary.main",
-          color: "white",
-          padding: 1,
-          borderRadius: 1,
-        }}
+        className="pit-stop-hover-area"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        <Typography variant="body2" className="pit-stop-type">
-          {pitStop.type}
-        </Typography>
-        <Typography variant="caption" className="pit-stop-time">
-          {pitStop.time} mins
-        </Typography>
+        <Box className="pit-stop-line" />
+        <Box
+          className={`pit-stop ${show ? "show" : ""} ${hover ? "hover" : ""}`}
+        >
+          <Typography variant="body2" className="pit-stop-type">
+            {pitStop.type}
+          </Typography>
+          <Typography variant="caption" className="pit-stop-time">
+            {pitStop.time} mins
+          </Typography>
+          <Typography variant="caption" className="avg-lap-time">
+            Avg Lap: {pitStop.avgLapTime} secs
+          </Typography>
+          <Typography variant="caption" className="pit-stop-time">
+            {calculateTime(pitStop.lap)}
+          </Typography>
+        </Box>
       </Box>
     </Rnd>
   );
--- src/components/RaceTrack.js
+++ src/components/RaceTrack.js
@@ -1,32 +1,80 @@
 import React, { useState } from "react";
 import PitStop from "./PitStop";
-import { Box, Paper } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import "../css/RaceTrack.css";
 
 const RaceTrack = () => {
   const [pitStops, setPitStops] = useState([
-    { id: 1, lap: 100, type: "tyres", time: 10 },
-    { id: 2, lap: 300, type: "fuel", time: 15 },
-    { id: 3, lap: 500, type: "driver", time: 20 },
+    { id: 1, lap: 4, type: "tyres", time: 10, avgLapTime: 120 },
+    { id: 2, lap: 8, type: "fuel", time: 15, avgLapTime: 125 },
+    { id: 3, lap: 12, type: "driver", time: 20, avgLapTime: 130 },
   ]);
 
   const handleDragStop = (e, d, id) => {
     const updatedPitStops = pitStops.map((pitStop) =>
-      pitStop.id === id ? { ...pitStop, lap: d.x } : pitStop
+      pitStop.id === id ? { ...pitStop, lap: (d.x / trackWidth) * 24 } : pitStop
     );
     setPitStops(updatedPitStops);
   };
 
+  const trackWidth = 800; // Width of the track in pixels
+
   return (
     <Paper elevation={3} className="race-track">
       <Box className="track-line" />
+      {Array.from({ length: 25 }, (_, i) => (
+        <Typography
+          key={i}
+          className="track-time"
+          style={{ left: `${(i / 25) * 100}%` }}
+        >
+          {i}:00
+        </Typography>
+      ))}
       {pitStops.map((pitStop) => (
         <PitStop
           key={pitStop.id}
           pitStop={pitStop}
           onDragStop={handleDragStop}
+          trackWidth={trackWidth}
         />
       ))}
+      <Box className="key-table">
+        <Typography variant="h6">Race Strategy Summary</Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {/* <TableCell>Metric</TableCell>
+                <TableCell>Value</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                {/* <TableCell>Total Laps</TableCell>
+                <TableCell>{pitStops[pitStops.length - 1]?.lap || 0}</TableCell> */}
+              </TableRow>
+              <TableRow>
+                {/* <TableCell>Total Fuel Used</TableCell>
+                <TableCell>
+                  {pitStops.reduce((sum, pitStop) => sum + pitStop.time, 0)}{" "}
+                  liters
+                </TableCell> */}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Paper>
   );
 };
