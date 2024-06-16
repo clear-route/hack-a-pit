6d4859756143fad52c84e29abd9c40ff21b032da
Matt Johnson <matt.johnson@clearroute.co.uk>
2024-06-16 13:30:59 +0100

    typo

--- src/components/CurrentStatus.js
+++ src/components/CurrentStatus.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
-const CurrentStatus = ({currentStatus}) => {
-
+const CurrentStatus = ({ currentStatus }) => {
   return (
     <Box
       sx={{
         padding: 2,
-        border: '1px solid #ccc',
+        border: "1px solid #ccc",
         borderRadius: 2,
         marginBottom: 2,
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
       }}
     >
       <Box sx={{ flex: 1, marginRight: 1 }}>
@@ -19,42 +18,63 @@ const CurrentStatus = ({currentStatus}) => {
         {currentStatus && (
           <Box>
             <Typography variant="body2">Lap: {currentStatus.lap}</Typography>
-            <Typography variant="body2">Track Temp: {currentStatus.trackTemperature}°C</Typography>
+            <Typography variant="body2">
+              Track Temp: {currentStatus.trackTemperature}°C
+            </Typography>
           </Box>
         )}
       </Box>
       <Box sx={{ flex: 1, marginLeft: 1 }}>
         {currentStatus && (
           <Box>
-            <Typography variant="h6">Car Overview
+            <Typography variant="h6">Car Overview</Typography>
+            <Typography variant="body2">
+              Fuel Remaining: {currentStatus.fuelRemaining}%
+            </Typography>
+            <Typography variant="body2">
+              Tire Health: {currentStatus.tireWear}%
+            </Typography>
+            <Typography variant="body2">
+              Engine Temperature: {currentStatus.engineTemperature}%
+            </Typography>
+            <Typography variant="body2">
+              Average Miles Per Gallon: {currentStatus.milesPerGallon}
+            </Typography>
+            {currentStatus.shouldPitStop && (
+              <Typography variant="body2" color="error">
+                Should Pit Stop: Yes
+              </Typography>
+            )}
+          </Box>
+        )}
+      </Box>
+      <Box sx={{ flex: 1, marginLeft: 1 }}>
+        {currentStatus && (
+          <>
+            <Typography variant="h6">Driver 1 Overview</Typography>
+            <Typography variant="body2">
+              Driver Stint Minutes: {currentStatus.driver1.stintMinutes}
+            </Typography>
+            <Typography variant="body2">
+              Driver Tiredness: {currentStatus.driver1.tiredness}%
             </Typography>
 
-            <Typography variant="body2">Fuel Remaining: {currentStatus.fuelRemaining}%</Typography>
-            <Typography variant="body2">Tire Health: {currentStatus.tireWear}%</Typography>
-            <Typography variant="body2">Engine Temperature: {currentStatus.engineTemperature}%</Typography>
-            <Typography variant="body2">Average Miles Per Gallon: {currentStatus.milesPerGallon}</Typography>
+            <Typography variant="h6">Driver 2 Overview</Typography>
+            <Typography variant="body2">
+              Driver Stint Minutes: {currentStatus.driver2.stintMinutes}
+            </Typography>
+            <Typography variant="body2">
+              Driver Tiredness: {currentStatus.driver2.tiredness}%
+            </Typography>
 
-            {currentStatus.shouldPitStop && (
-              <Typography variant="body2" color="error">
-                Should Pit Stop: Yes
-              </Typography>
-            )}
-          </Box>
-        )}
-      </Box>
-      <Box sx={{ flex: 1, marginLeft: 1 }}>
-        {currentStatus && (
-          <Box>
-            <Typography variant="h6">Driver Overview</Typography>
-
-            <Typography variant="body2">Driver Stint Minutes: {currentStatus.stintMinutes}</Typography>
-            <Typography variant="body2">Driver Tiredness : {currentStatus.driverTiredness}%</Typography>
-            {currentStatus.shouldPitStop && (
-              <Typography variant="body2" color="error">
-                Should Pit Stop: Yes
-              </Typography>
-            )}
-          </Box>
+            <Typography variant="h6">Driver 3 Overview</Typography>
+            <Typography variant="body2">
+              Driver Stint Minutes: {currentStatus.driver3.stintMinutes}
+            </Typography>
+            <Typography variant="body2">
+              Driver Tiredness: {currentStatus.driver3.tiredness}%
+            </Typography>
+          </>
         )}
       </Box>
     </Box>
--- src/components/PitStopGraph.js
+++ src/components/PitStopGraph.js
@@ -1,108 +1,170 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
-import annotationPlugin from 'chartjs-plugin-annotation';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+import annotationPlugin from "chartjs-plugin-annotation";
 import { Box } from "@mui/material";
 
 Chart.register(...registerables, annotationPlugin);
 
 const PitStopGraph = ({ pitStopTimes, currentTime }) => {
   const times = Object.keys(pitStopTimes);
-  const labels = times.map(time => time);
+  const labels = times.map((time) => time);
 
-  const trackTemperatureData = times.map(time => pitStopTimes[time].trackTemperature);
-  const fuelRemainingData = times.map(time => pitStopTimes[time].fuelRemaining);
-  const tireWearData = times.map(time => pitStopTimes[time].tireWear);
-  const engineTemperatureData = times.map(time => pitStopTimes[time].engineTemperature);
-  const driverTirednessData = times.map(time => pitStopTimes[time].driverTiredness);
+  const trackTemperatureData = times.map(
+    (time) => pitStopTimes[time].trackTemperature
+  );
+  const fuelRemainingData = times.map(
+    (time) => pitStopTimes[time].fuelRemaining
+  );
+  const tireWearData = times.map((time) => pitStopTimes[time].tireWear);
+  const engineTemperatureData = times.map(
+    (time) => pitStopTimes[time].engineTemperature
+  );
+  const driverTirednessData1 = times.map(
+    (time) => pitStopTimes[time].driver1.tiredness
+  );
+  const driverTirednessData2 = times.map(
+    (time) => pitStopTimes[time].driver2.tiredness
+  );
+  const driverTirednessData3 = times.map(
+    (time) => pitStopTimes[time].driver3.tiredness
+  );
 
   const chartData = {
     labels,
     datasets: [
       {
-        label: 'Track Temperature (°C)',
+        label: "Track Temperature (°C)",
         data: trackTemperatureData,
-        borderColor: 'red',
+        borderColor: "red",
         fill: false,
       },
       {
-        label: 'Fuel Remaining (%)',
+        label: "Fuel Remaining (%)",
         data: fuelRemainingData,
-        borderColor: 'brown',
+        borderColor: "brown",
         fill: false,
       },
       {
-        label: 'Tire Wear (%)',
+        label: "Tire Wear (%)",
         data: tireWearData,
-        borderColor: 'orange',
+        borderColor: "orange",
         fill: false,
       },
       {
-        label: 'Engine Temperature (°C)',
+        label: "Engine Temperature (°C)",
         data: engineTemperatureData,
-        borderColor: 'purple',
+        borderColor: "purple",
         fill: false,
       },
       {
-        label: 'Driver Tiredness (%)',
-        data: driverTirednessData,
-        borderColor: 'pink',
+        label: "Driver 1 Tiredness (%)",
+        data: driverTirednessData1,
+        borderColor: "blue",
+        fill: false,
+      },
+      {
+        label: "Driver 2 Tiredness (%)",
+        data: driverTirednessData2,
+        borderColor: "yellow",
+        fill: false,
+      },
+      {
+        label: "Driver 3 Tiredness (%)",
+        data: driverTirednessData3,
+        borderColor: "pink",
         fill: false,
       },
     ],
   };
 
-  const currentTimeString = `${String(currentTime.getHours()).padStart(2, '0')}:${String(currentTime.getMinutes()).padStart(2, '0')}`;
+  const currentTimeString = `${String(currentTime.getHours()).padStart(
+    2,
+    "0"
+  )}:${String(currentTime.getMinutes()).padStart(2, "0")}`;
   const annotations = [
     {
-      type: 'line',
-      mode: 'vertical',
-      scaleID: 'x',
+      type: "line",
+      mode: "vertical",
+      scaleID: "x",
       value: currentTimeString,
-      borderColor: 'black',
+      borderColor: "black",
       borderWidth: 2,
       label: {
         display: true,
-        content: 'Elapsed Time',
+        content: "Elapsed Time",
         enabled: true,
-        position: 'start',
-        backgroundColor: 'black', // Adjust background color for readability
+        position: "start",
+        backgroundColor: "black", // Adjust background color for readability
         yAdjust: -0, // Position label above the chart area
         z: 1000,
         opacity: 1.0,
-        height: 10
-        
+        height: 10,
       },
-
-      
     },
     ...Object.keys(pitStopTimes)
-      .filter(time => pitStopTimes[time].shouldPitStop)
-      .map(time => ({
-        type: 'line',
-        mode: 'vertical',
-        scaleID: 'x',
+      .filter((time) => pitStopTimes[time].shouldPitStop)
+      .map((time) => ({
+        type: "line",
+        mode: "vertical",
+        scaleID: "x",
         value: time,
-        borderColor: 'green',
+        borderColor: "green",
         borderWidth: 2,
         label: {
-          content: 'Pit Stop Recommended',
+          content: [
+            "Pit Stop Recommended",
+            "- Refuel",
+            "- Change Tires",
+            "- Change Drivers",
+          ],
           enabled: true,
           display: true,
-          position: 'start',
-          backgroundColor: 'green', // Adjust background color for readability
+          position: "start",
+          backgroundColor: "green", // Adjust background color for readability
           yAdjust: -0, // Position label above the chart area
           z: 1000,
           opacity: 1.0,
-          height: 10
+          height: 10,
         },
       })),
     {
-      type: 'box',
+      type: "box",
       xMin: labels[0],
       xMax: currentTimeString,
-      backgroundColor: 'rgba(211, 211, 211, 0.5)', // Light grey shading for actual data
+      backgroundColor: "rgba(211, 211, 211, 0.5)",
     },
+    ...times
+      .map((time, index) => {
+        const previousTime = index > 0 ? times[index - 1] : null;
+        if (
+          previousTime &&
+          (pitStopTimes[time].driver1.stintMinutes <
+            pitStopTimes[previousTime].driver1.stintMinutes ||
+            pitStopTimes[time].driver2.stintMinutes <
+              pitStopTimes[previousTime].driver2.stintMinutes ||
+            pitStopTimes[time].driver3.stintMinutes <
+              pitStopTimes[previousTime].driver3.stintMinutes)
+        ) {
+          return {
+            type: "line",
+            mode: "vertical",
+            scaleID: "x",
+            value: time,
+            borderColor: "blue",
+            borderWidth: 2,
+            label: {
+              content: "Driver Change",
+              enabled: true,
+              position: "start",
+              backgroundColor: "rgba(0, 0, 255, 0.8)",
+              yAdjust: -20,
+            },
+          };
+        }
+        return null;
+      })
+      .filter((annotation) => annotation !== null),
   ];
 
   const options = {
@@ -110,13 +172,13 @@ const PitStopGraph = ({ pitStopTimes, currentTime }) => {
       x: {
         title: {
           display: true,
-          text: 'Time',
+          text: "Time",
         },
       },
       y: {
         title: {
           display: true,
-          text: 'Value',
+          text: "Value",
         },
         beginAtZero: true,
       },
@@ -124,11 +186,11 @@ const PitStopGraph = ({ pitStopTimes, currentTime }) => {
     plugins: {
       legend: {
         display: true,
-        position: 'bottom', // Position the legend below the graph
+        position: "bottom", // Position the legend below the graph
       },
       annotation: {
         annotations,
-        drawTime: 'afterDatasetsDraw', // Ensures annotations are drawn after the datasets
+        drawTime: "afterDatasetsDraw",
       },
     },
   };
@@ -136,10 +198,10 @@ const PitStopGraph = ({ pitStopTimes, currentTime }) => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        overflowX: 'scroll',
+        display: "flex",
+        overflowX: "scroll",
         padding: 2,
-        border: '1px solid #ccc',
+        border: "1px solid #ccc",
         borderRadius: 2,
         marginX: 0,
         paddingY: 5,
