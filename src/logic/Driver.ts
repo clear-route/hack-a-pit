242f1e11e2b6f2cafb915130dd98aeb4d1c4b2b3
Lucas Marchi <lucas.marchi@clearroute.io>
2024-06-16 01:47:13 +0100

    fixed errors in functions

--- src/logic/Driver.ts
+++ src/logic/Driver.ts
@@ -1,22 +1,36 @@
 export class Driver {
-  private driverRemainingTimeInMinutes: number;
+  private racingTimeLeft: number;
   private healthLeft: number = 100;
 
-  constructor(private name: string, private timeToExitRaceInMiutes: number) {
+  constructor(private name: string, private timeToExitRaceInMinutes: number) {
     this.name = name;
-    this.driverRemainingTimeInMinutes = this.timeToExitRaceInMiutes;
+    this.racingTimeLeft = this.timeToExitRaceInMinutes;
   }
 
   public getHealthLeft(): number {
     return this.healthLeft;
   }
 
-  public upHealthLeftBy(amount: number): void {
+  public upHealthBy(amount: number): void {
     this.healthLeft =
       this.healthLeft + amount <= 100 ? this.healthLeft + amount : 100;
   }
 
+  public reduceHealthBy(amount: number): void {
+    this.healthLeft =
+      this.healthLeft - amount >= 0 ? this.healthLeft - amount : 0;
+  }
+
   public getDriverRemainingTimeInMinutes(): number {
-    return this.driverRemainingTimeInMinutes;
+    return this.racingTimeLeft;
+  }
+
+  public reduceRacingTimeLeftBy(amount: number): void {
+    this.racingTimeLeft =
+      this.racingTimeLeft - amount >= 0 ? this.racingTimeLeft - amount : 0;
+  }
+
+  public getDriverName(): string {
+    return this.name;
   }
 }
